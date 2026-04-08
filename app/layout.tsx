3998f6fe85f58cfa6300bc160b99113245c7ffdc
Ivan Kireev <31880548+sm4chnik@users.noreply.github.com>
2026-04-08 17:18:58 -0300

    add fonts and colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <head>
+        <link rel="stylesheet" href="/fonts/stylesheet.css" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
