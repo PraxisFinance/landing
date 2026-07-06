c6e5a60b2a8883cec44dd5dab248d89b038784bc
aii23 <a.ivlev.om@gmail.com>
2026-07-06 15:02:09 +0300

    Revert "Added banner about social"
    This reverts commit eaee73b3f41198d97eefb1b1fda323198e330d5e.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { AboutSection } from "@/components/sections/about/AboutSection";
-import { AnnouncementBanner } from "@/components/sections/AnnouncementBanner";
 import { CoverSection } from "@/components/sections/cover/CoverSection";
 import { FAQSection } from "@/components/sections/faq/FAQSection";
 import { JoinWaitlistSection } from "@/components/sections/join-waitlist/JoinWaitlistSection";
@@ -13,7 +12,6 @@ import { UserFlowSection } from "@/components/sections/user-flow/UserFlowSection
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-brand-white text-brand-black">
-      <AnnouncementBanner />
       <LandingHeader />
 
       <main className="mb-[100px] flex flex-col gap-[100px] md:mb-[180px] md:gap-[180px] px-5 sm:px-6 lg:px-10">
